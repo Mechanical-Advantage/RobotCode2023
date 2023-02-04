5b3ac3d0b316c434296ec932dba8355c3ef6a063
Jonah <47046556+jonahb55@users.noreply.github.com>
2023-02-03 23:49:08 -0500

    Silence joystick connection warning in RobotContainer test

--- src/test/java/org/littletonrobotics/frc2023/RobotContainerTest.java
+++ src/test/java/org/littletonrobotics/frc2023/RobotContainerTest.java
@@ -7,6 +7,7 @@
 
 package org.littletonrobotics.frc2023;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import org.junit.jupiter.api.Test;
 import org.littletonrobotics.junction.inputs.LoggedDriverStation;
 
@@ -18,6 +19,7 @@ public class RobotContainerTest {
     LoggedDriverStation.getInstance().getJoystickData(0).xbox = true;
     LoggedDriverStation.getInstance().getJoystickData(1).xbox = true;
     LoggedDriverStation.getInstance().getJoystickData(2).name = "Generic   USB  Joystick";
+    DriverStation.silenceJoystickConnectionWarning(true);
 
     // Instantiate RobotContainer
     new RobotContainer();
