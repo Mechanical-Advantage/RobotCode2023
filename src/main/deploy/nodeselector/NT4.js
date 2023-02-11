8df75577f19614d207f8ee25a5e4d4a8bd928a47
Jonah <47046556+jonahb55@users.noreply.github.com>
2023-02-11 12:24:56 -0500

    Add license headers to node selector files

--- src/main/deploy/nodeselector/NT4.js
+++ src/main/deploy/nodeselector/NT4.js
@@ -1,3 +1,10 @@
+// Copyright (c) 2023 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 import { serialize, deserialize } from "./msgpack.js";
 const typestrIdxLookup = {
   boolean: 0,
--- src/main/deploy/nodeselector/index.js
+++ src/main/deploy/nodeselector/index.js
@@ -1,3 +1,10 @@
+// Copyright (c) 2023 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 import { NT4_Client } from "./NT4.js";
 
 const robotToDashboardTopic = "/nodeselector/robot_to_dashboard";
