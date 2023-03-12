45ede2648629eaee5607f983ffb10477c544e0ea
Jonah <47046556+jonahb55@users.noreply.github.com>
2023-03-12 13:28:07 -0400

    Update at "3/12/2023, 1:28:07 PM"

--- src/main/java/org/littletonrobotics/frc2023/commands/AutoScore.java
+++ src/main/java/org/littletonrobotics/frc2023/commands/AutoScore.java
@@ -10,8 +10,10 @@ package org.littletonrobotics.frc2023.commands;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -26,6 +28,14 @@ import org.littletonrobotics.frc2023.util.AllianceFlipUtil;
 import org.littletonrobotics.frc2023.util.GeomUtil;
 
 public class AutoScore extends SequentialCommandGroup {
+  public static final double bendCompensation =
+      Units.inchesToMeters(-1.5); // Drive Y offset in inches on the front
+
+  public static Transform2d getBendCompensation(boolean isFront) {
+    return new Transform2d(
+        new Translation2d(0.0, bendCompensation * (isFront ? 1.0 : -1.0)), new Rotation2d());
+  }
+
   public static final double minDriveX = FieldConstants.Grids.outerX + 0.45;
   public static final double minDriveY = 0.7;
   public static final double maxDriveY = FieldConstants.Community.leftY - 0.5;
@@ -180,10 +190,12 @@ public class AutoScore extends SequentialCommandGroup {
 
     // If reach not allowed, return target at minimum distance
     if (!allowReach) {
+      boolean front = shouldScoreFront(pose, objective);
       return new Pose2d(
-          Math.max(minDriveX, nodeTranslation.getX() + minDistance),
-          nodeTranslation.getY(),
-          Rotation2d.fromDegrees(shouldScoreFront(pose, objective) ? 180.0 : 0.0));
+              Math.max(minDriveX, nodeTranslation.getX() + minDistance),
+              nodeTranslation.getY(),
+              Rotation2d.fromDegrees(front ? 180.0 : 0.0))
+          .transformBy(getBendCompensation(front));
     }
 
     // Calculate angle from node
@@ -217,7 +229,8 @@ public class AutoScore extends SequentialCommandGroup {
             .minus(driveTranslation)
             .getAngle()
             .plus(Rotation2d.fromDegrees(shouldScoreFront(pose, objective) ? 0.0 : 180.0));
-    return new Pose2d(driveTranslation, driveRotation);
+    return new Pose2d(driveTranslation, driveRotation)
+        .transformBy(getBendCompensation(shouldScoreFront(pose, objective)));
   }
 
   /** Returns the best arm target for the selected node and drive position. */
