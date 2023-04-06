c2a43d31a75c6ee6c3958159feb92a1af1cc69a0
Jonah <47046556+jonahb55@users.noreply.github.com>
2023-04-06 12:32:04 -0400

    Update at "4/6/2023, 12:32:04 PM"

--- src/main/java/org/littletonrobotics/frc2023/subsystems/gripper/Gripper.java
+++ src/main/java/org/littletonrobotics/frc2023/subsystems/gripper/Gripper.java
@@ -29,7 +29,7 @@ public class Gripper extends SubsystemBase {
   private final GripperIOInputsAutoLogged inputs = new GripperIOInputsAutoLogged();
 
   private static final LoggedTunableNumber holdVolts =
-      new LoggedTunableNumber("Gripper/HoldVolts", 1.5);
+      new LoggedTunableNumber("Gripper/HoldVolts", 1.2);
   private static final LoggedTunableNumber intakeVolts =
       new LoggedTunableNumber("Gripper/IntakeVolts", 12.0);
   private static final LoggedTunableNumber intakeVelocityWaitStart =
