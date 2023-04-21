d4f3cf9ac07863adc02f8f220ae25c917e6bf31c
Jonah <47046556+jonahb55@users.noreply.github.com>
2023-04-21 09:41:56 -0500

    Update at "4/21/2023, 9:41:56 AM"

--- src/main/java/org/littletonrobotics/frc2023/commands/DriveToPose.java
+++ src/main/java/org/littletonrobotics/frc2023/commands/DriveToPose.java
@@ -69,7 +69,7 @@ public class DriveToPose extends CommandBase {
         thetaKp.initDefault(5.0);
         thetaKd.initDefault(0.0);
         driveMaxVelocity.initDefault(Units.inchesToMeters(150.0));
-        driveMaxAcceleration.initDefault(Units.inchesToMeters(120.0));
+        driveMaxAcceleration.initDefault(Units.inchesToMeters(95.0));
         thetaMaxVelocity.initDefault(Units.degreesToRadians(360.0));
         thetaMaxAcceleration.initDefault(Units.degreesToRadians(720.0));
         driveTolerance.initDefault(0.01);
