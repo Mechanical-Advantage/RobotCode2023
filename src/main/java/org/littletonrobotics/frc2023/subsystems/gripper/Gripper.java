6e7a82613f977c4a7284fee22ee563e89dffc6f5
Jonah <47046556+jonahb55@users.noreply.github.com>
2023-06-01 19:56:29 -0400

    Lower gripper hold volts

--- src/main/java/org/littletonrobotics/frc2023/subsystems/gripper/Gripper.java
+++ src/main/java/org/littletonrobotics/frc2023/subsystems/gripper/Gripper.java
@@ -30,7 +30,7 @@ public class Gripper extends SubsystemBase {
   private final GripperIOInputsAutoLogged inputs = new GripperIOInputsAutoLogged();
 
   private static final LoggedTunableNumber holdVolts =
-      new LoggedTunableNumber("Gripper/HoldVolts", 1.35);
+      new LoggedTunableNumber("Gripper/HoldVolts", 0.8);
   private static final LoggedTunableNumber intakeVolts =
       new LoggedTunableNumber("Gripper/IntakeVolts", 12.0);
   private static final LoggedTunableNumber intakeVelocityWaitStart =
